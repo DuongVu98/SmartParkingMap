43526dfe5551e40efbaa8a78b23ecd209d1bdb58
Vũ Tùng Dương <duongvutung1998@gmail.com>
2018-12-15 01:02:43 +0700

    add filter for jdbc

--- src/main/java/hcmiu/iot/smartparking/servlets/Map.java
+++ src/main/java/hcmiu/iot/smartparking/servlets/Map.java
@@ -14,6 +14,7 @@ import hcmiu.iot.smartparking.beans.Place;
 import hcmiu.iot.smartparking.dao.PlacesDAO;
 import hcmiu.iot.smartparking.utils.MyUtils;
 
+
 public class Map extends HttpServlet{
 	private static final long serialVersionUID=1L;
 	private ArrayList<Place> pList=null;
---
+++ src/main/java/hcmiu/iot/smartparking/utils/JDBCFilter.java
@@ -0,0 +1,63 @@
+package hcmiu.iot.smartparking.utils;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+import hcmiu.iot.smartparking.conn.ConnectionUtils;
+
+@WebFilter(urlPatterns= {"/*"})
+public class JDBCFilter implements Filter{
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		HttpServletRequest req=(HttpServletRequest)request;
+		String servletPath= req.getServletPath();
+		
+		if(servletPath.contains("/initmap")) {
+			Connection conn=null;
+			
+			try {
+				conn=ConnectionUtils.getConnection();
+				conn.setAutoCommit(false);
+				MyUtils.storeConnection(request, conn);
+				chain.doFilter(request, response);
+				conn.commit();
+				
+			} catch (ClassNotFoundException | SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				ConnectionUtils.rollbackQuietly(conn);
+				throw new ServletException();
+			}finally {
+				ConnectionUtils.closeQuietly(conn);
+			}
+		}else {
+			chain.doFilter(request, response);
+		}
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
