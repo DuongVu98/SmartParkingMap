c721b57df3ae0fa3630b84481bd5404b0f17318f
Vũ Tùng Dương <duongvutung1998@gmail.com>
2018-12-14 22:27:35 +0700

    add myutils

---
+++ src/main/java/hcmiu/iot/smartparking/utils/MyUtils.java
@@ -0,0 +1,20 @@
+package hcmiu.iot.smartparking.utils;
+
+import java.sql.Connection;
+
+import javax.servlet.ServletRequest;
+
+public class MyUtils {
+	public static final String ATT_NAME_CONNECTION="MY_CONNECTION_ATTRIBUTE";
+	
+	public static void storeConnection(ServletRequest request, Connection conn) {
+		request.setAttribute(ATT_NAME_CONNECTION, conn);
+	}
+	
+	//Connection utilities
+	public static Connection getConnection(ServletRequest request, Connection conn) {
+		return (Connection)request.getAttribute(ATT_NAME_CONNECTION);
+	}
+	
+	
+}
--- src/main/java/hcmiu/iot/smartparking/utils/PlacesUtils.java
+++ src/main/java/hcmiu/iot/smartparking/utils/PlacesUtils.java
@@ -1,5 +1,14 @@
 package hcmiu.iot.smartparking.utils;
 
-public interface PlacesUtils {
+import java.sql.Connection;
+import java.util.List;
 
+import hcmiu.iot.smartparking.servlets.Place;
+
+public interface PlacesUtils {
+	public Place getById(Connection conn, int id);
+	public void savePlace(Connection conn, Place place);
+	public void updatePlace(Connection conn, Place place);
+	public void delete(Connection conn, int id);
+	public List<Place> getAllPlaces(Connection conn);
 }
